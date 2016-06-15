b5bb35d24ae90dbd725366050fc5cf4805cee9c9
Jon Barlow <orviwan@gmail.com>
2016-06-15 06:44:31 -0700

    intial

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # watchface-tutorial-unobstructed
+
 An example of the UnobstructedArea API
+
+## Guide
+
+This example is part of the [Unobstructed Area Guide](https://developer.pebble.com/guides/user-interfaces/unobstructed-area)
+on the Pebble Developer Site.
+
+## License
+
+This project is licensed under the [MIT License](./LICENSE).
---
+++ src/watchface-tutorial.c
@@ -0,0 +1,225 @@
+#include <pebble.h>
+
+#define KEY_TEMPERATURE 0
+#define KEY_CONDITIONS  1
+
+static Window *s_main_window;
+static Layer *window_layer;
+static TextLayer *s_time_layer;
+static TextLayer *s_weather_layer;
+
+static GFont s_time_font;
+static GFont s_weather_font;
+
+static BitmapLayer *s_background_layer;
+static GBitmap *s_background_bitmap;
+
+static const uint8_t s_time_offset_top_percent = 31;
+static const uint8_t s_weather_offset_top_percent = 76;
+
+uint8_t relative_pixel(uint8_t percent, uint8_t max) {
+  return (max * percent) / 100;
+}
+
+static void update_ui(void) {
+  // Adapt the layout based on any obstructions
+  GRect full_bounds = layer_get_bounds(window_layer);
+  GRect visible_bounds = layer_get_unobstructed_bounds(window_layer);
+
+  if (!grect_equal(&full_bounds, &visible_bounds)) {
+    // Screen is obstructed
+    layer_set_hidden(bitmap_layer_get_layer(s_background_layer), true);
+    text_layer_set_text_color(s_time_layer, GColorWhite);
+  } else {
+    // Screen is unobstructed
+    layer_set_hidden(bitmap_layer_get_layer(s_background_layer), false);
+    text_layer_set_text_color(s_time_layer, GColorBlack);
+  }
+
+  GRect time_frame = layer_get_frame(text_layer_get_layer(s_time_layer));
+  time_frame.origin.y = relative_pixel(s_time_offset_top_percent, visible_bounds.size.h);
+  layer_set_frame(text_layer_get_layer(s_time_layer), time_frame);
+
+  GRect weather_frame = layer_get_frame(text_layer_get_layer(s_weather_layer));
+  weather_frame.origin.y = relative_pixel(s_weather_offset_top_percent, visible_bounds.size.h);
+  layer_set_frame(text_layer_get_layer(s_weather_layer), weather_frame);
+}
+
+static void initialise_ui(void) {
+  GRect bounds = layer_get_bounds(window_layer);
+
+  // Create GBitmap
+  s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BACKGROUND);
+
+  // Create BitmapLayer to display the GBitmap
+  s_background_layer = bitmap_layer_create(bounds);
+  bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
+  layer_add_child(window_layer, bitmap_layer_get_layer(s_background_layer));
+
+  // Create time TextLayer
+  s_time_layer = text_layer_create(GRect(0,
+    relative_pixel(s_time_offset_top_percent, bounds.size.h), bounds.size.w, 50));
+  text_layer_set_background_color(s_time_layer, GColorClear);
+  text_layer_set_text_color(s_time_layer, GColorBlack);
+  text_layer_set_text(s_time_layer, "00:00");
+
+  // Create GFont
+  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PERFECT_DOS_48));
+
+  // Apply to TextLayer
+  text_layer_set_font(s_time_layer, s_time_font);
+  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+  layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
+
+  // Create temperature Layer
+  s_weather_layer = text_layer_create(GRect(0,
+    relative_pixel(s_weather_offset_top_percent, bounds.size.h), bounds.size.w, 25));
+  text_layer_set_background_color(s_weather_layer, GColorClear);
+  text_layer_set_text_color(s_weather_layer, GColorWhite);
+  text_layer_set_text_alignment(s_weather_layer, GTextAlignmentCenter);
+  text_layer_set_text(s_weather_layer, "Loading...");
+
+  // Create second custom font, apply it and add to Window
+  s_weather_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PERFECT_DOS_20));
+  text_layer_set_font(s_weather_layer, s_weather_font);
+  layer_add_child(window_layer, text_layer_get_layer(s_weather_layer));
+
+  // Check for obstructions
+  update_ui();
+}
+
+static void destroy_ui(void) {
+  gbitmap_destroy(s_background_bitmap);
+  bitmap_layer_destroy(s_background_layer);
+  text_layer_destroy(s_time_layer);
+  text_layer_destroy(s_weather_layer);
+}
+
+static void update_time(void) {
+ // Get a tm structure
+ time_t temp = time(NULL);
+ struct tm *tick_time = localtime(&temp);
+
+ // Write the current hours and minutes into a buffer
+ static char s_buffer[8];
+ strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ? "%H:%M" : "%I:%M", tick_time);
+
+ // Display this time on the TextLayer
+ text_layer_set_text(s_time_layer, s_buffer);
+}
+
+static void update_weather(void) {
+  DictionaryIterator *iter;
+  app_message_outbox_begin(&iter);
+  dict_write_uint8(iter, 0, 0);
+  app_message_outbox_send();
+}
+
+static void app_unobstructed_change(AnimationProgress progress, void *context) {
+  update_ui();
+}
+
+static void main_window_load(Window *window) {
+  window_layer = window_get_root_layer(window);
+
+  // Create the UI elements
+  initialise_ui();
+
+  // Make sure the time is displayed from the start
+  update_time();
+
+  // Subscribe to the change event
+  UnobstructedAreaHandlers handlers = {
+    .change = app_unobstructed_change
+  };
+  unobstructed_area_service_subscribe(handlers, NULL);
+
+}
+
+static void main_window_unload(Window *window) {
+  fonts_unload_custom_font(s_time_font);
+  fonts_unload_custom_font(s_weather_font);
+
+  unobstructed_area_service_unsubscribe();
+
+  // Clean up the unused UI elenents
+  destroy_ui();
+}
+
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+  update_time();
+
+  // Get weather update every 30 minutes
+  if(tick_time->tm_min % 30 == 0) {
+    update_weather();
+  }
+}
+
+static void inbox_received_callback(DictionaryIterator *iterator, void *context) {
+  // Store incoming information
+  static char temperature_buffer[8];
+  static char conditions_buffer[32];
+  static char weather_layer_buffer[32];
+
+  // Read tuples for data
+  Tuple *temp_tuple = dict_find(iterator, MESSAGE_KEY_Temperature);
+  Tuple *conditions_tuple = dict_find(iterator, MESSAGE_KEY_Conditions);
+
+  // If all data is available, use it
+  if(temp_tuple && conditions_tuple) {
+    snprintf(temperature_buffer, sizeof(temperature_buffer), "%dC", (int)temp_tuple->value->int32);
+    snprintf(conditions_buffer, sizeof(conditions_buffer), "%s", conditions_tuple->value->cstring);
+
+    // Assemble full string and display
+    snprintf(weather_layer_buffer, sizeof(weather_layer_buffer), "%s, %s", temperature_buffer, conditions_buffer);
+    text_layer_set_text(s_weather_layer, weather_layer_buffer);
+  }
+}
+
+static void inbox_dropped_callback(AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Message dropped!");
+}
+
+static void outbox_failed_callback(DictionaryIterator *iterator, AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Outbox send failed!");
+}
+
+static void outbox_sent_callback(DictionaryIterator *iterator, void *context) {
+  APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
+}
+
+static void init() {
+  // Create main Window element and assign to pointer
+  s_main_window = window_create();
+  window_set_background_color(s_main_window, GColorBlack);
+  window_set_window_handlers(s_main_window, (WindowHandlers) {
+    .load = main_window_load,
+    .unload = main_window_unload
+  });
+  window_stack_push(s_main_window, true);
+
+  // Register with TickTimerService
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+
+  // Register callbacks
+  app_message_register_inbox_received(inbox_received_callback);
+  app_message_register_inbox_dropped(inbox_dropped_callback);
+  app_message_register_outbox_failed(outbox_failed_callback);
+  app_message_register_outbox_sent(outbox_sent_callback);
+
+  // Open AppMessage
+  const int inbox_size = 128;
+  const int outbox_size = 128;
+  app_message_open(inbox_size, outbox_size);
+}
+
+static void deinit() {
+  // Destroy Window
+  window_destroy(s_main_window);
+}
+
+int main(void) {
+  init();
+  app_event_loop();
+  deinit();
+}
